73b2db465cd6b5088928a66cf09ec6e5172ad234
Muaaz852 <43746049+Muaaz852@users.noreply.github.com>
2018-10-03 15:28:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return '<h1>ALHAMDULILLAH</h1>'
 
+@app.route('/muaaz')
+def magic():
+    return '<h1>ALHAMDULILLAH</h1> <h2>MuAaZ</h2>'
+
 if __name__ == '__main__':
     app.run()
